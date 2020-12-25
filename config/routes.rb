79c6793ef469d626b136d6eb0404b0e4ee7fb775
sandobits <sandbar.dev@gmail.com>
2020-12-25 14:06:47 -0200

    Add Articles#show action and view

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   root 'pages#home'
   get '/about', to: 'pages#about'
+  
+  resources :articles, only: [:show] # selects only the #show action for a single article
+  # resources :articles
 end
