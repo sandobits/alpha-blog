90077472f9367882f366d44cb358d8e78bebacbf
sandobits <sandbar.dev@gmail.com>
2020-12-25 14:38:16 -0200

    Add Articles#index scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,5 @@ Rails.application.routes.draw do
   root 'pages#home'
   get '/about', to: 'pages#about'
   
-  resources :articles, only: [:show] # selects only the #show action for a single article
-  # resources :articles
+  resources :articles
 end
