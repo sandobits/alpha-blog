8c9bc57e2b5149752bd79b48550fde77d4f14a5f
sandobits <sandbar.dev@gmail.com>
2020-12-25 22:27:45 -0200

    Add Articles#create action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,4 +6,13 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+
+  def new 
+  end
+
+  def create
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    @article.save # will return false without error message if fields do not match validation
+    redirect_to @article
+  end
 end
